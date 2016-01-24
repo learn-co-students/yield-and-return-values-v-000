fcd62f9ad1d3b711f636f9fef860f6efddd671b3
lamass <hadzy@yahoo.com>
2016-01-24 17:30:35 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,23 @@
 require 'pry'
 
+
 def hello(array)
   i = 0
-  while i < array.length
+   collection = []
+  while i < array.length  
     yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
+     binding.pry
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name} " }
+
+# hello(arr) do |name|
+#   if name.start_with?("J")
+#     puts "Careful with your trade  #{name} "
+#   end
+# end
