db316d1d27907162b7d7c0c76585e822f68086e5
CodeByLine <yleventhal@ymail.com>
2016-02-17 03:07:48 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,5 +8,16 @@ def hello(array)
   end
 end
 
+binding.pry
+#hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+#Save the return value of yield(array[i])
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+end
