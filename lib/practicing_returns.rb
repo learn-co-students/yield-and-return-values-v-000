eb5bf79329aefae6eb4bf8bde1a8c910d6e95039
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-05-04 17:23:52 -0400

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,34 @@
+#require 'pry'
+
+#def hello(array)
+  #i = 0
+  #while i < array.length
+    
+    #yield(array[i])
+    #i += 1
+  #end
+#end
+
+#binding.pry
+#hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+#hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" } # <-- change to the code block  to capture the return value 
+
+
+# updated code from above 
+
 require 'pry'
 
 def hello(array)
   i = 0
-  while i < array.length
+  collection = []
+  while i < array.length #---> push the return value using 'Yield(array[i])' Into the Collection array 
     yield(array[i])
     i += 1
   end
+  collection #---> returning the new collection
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+#hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
