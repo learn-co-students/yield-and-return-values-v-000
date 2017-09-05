9fe8f0aab546dd9f32b2cd3abbf49d68011e807a
aisayo <aisayo@icloud.com>
2017-09-05 22:06:50 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,13 @@ require 'pry'
 
 def hello(array)
   i = 0
+  colletion = [ ] #setting this variable equal to empty array
   while i < array.length
-    yield(array[i])
+    collection << yield (array[i]) #push the return value of using yield(array[i]) into collection
     i += 1
   end
+  collection #need to return the value of new array
 end
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+#hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
