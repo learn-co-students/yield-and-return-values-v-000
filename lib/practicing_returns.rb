6c0a954b3960e4e805d2ed8c604a4cf3aac6e7c9
Pamela Frieze <pfrieze@gmail.com>
2017-10-08 00:57:54 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,16 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+#Arrays are great for storing lists of information
+#Block is code inside do and end or in-line between { and }
+#Return value of a while loop is always nil
