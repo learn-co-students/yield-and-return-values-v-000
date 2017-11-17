9cc9fe2399014e3551626693701ffad0b44d40b8
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-11-17 01:52:09 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,14 @@
-require 'pry'
+require 'pry' #We've built a collect iterator
 
 def hello(array)
-  i = 0
+  collection = [] #Creates a new array to store the values in
+  i = 0 #This starts the counter
   while i < array.length
-    yield(array[i])
-    i += 1
+    collection << yield(array[i]) #This pushes the value of the array index into the collection array. Yield allows us to push value in.
+    i += 1 #This keeps track of the counter and will stop the while loop.
   end
+  collection #this returns the new array.
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
