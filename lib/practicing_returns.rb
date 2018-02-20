786fc70f03833c30fb4c3797cc27183f1fc5cb31
Katie Cassell <kcassell944@gmail.com>
2018-02-20 23:14:26 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,5 +8,13 @@ def hello(array)
   end
 end
 
+def hello_t(array)
+  yield array
+end
+
+hello_t([1, 2, 5]) do |element|
+  element
+end
+
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
