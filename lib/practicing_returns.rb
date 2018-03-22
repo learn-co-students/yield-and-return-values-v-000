78d60de4fccb9b0f29c8f72b96f41ff14f60288a
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-22 05:06:50 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,18 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
+    #Shoveling/Pushing the name to the collection while writing
     i += 1
   end
+  collection
 end
+
+#Marcel Galang tried doing this ^
+#Instead of explaining to me -> He tried moving me forward
+#because the answers were the next lesson.
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
