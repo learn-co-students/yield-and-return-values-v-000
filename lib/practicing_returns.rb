bf108d2ae499169a9d71aacac6e466ac8bfcfe1d
David Moore <hey@iamdavidmoore.com>
2018-04-19 14:50:10 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,14 @@
 require 'pry'
 
+#this is an under-the-hood example of how the #collection method works
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
